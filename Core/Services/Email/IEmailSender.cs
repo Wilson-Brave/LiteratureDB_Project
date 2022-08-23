af4634238a64470e05917cbb8ae918cfc695bc70
Brave Wilson  Kahweka <bravekahweka@gmail.com>
2022-08-23 09:50:34 +0200

    set up

---
+++ Core/Services/Email/IEmailSender.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+namespace Core.Services.Email
+{
+	public interface IEmailSender
+	{
+		Task SendEmailAsync(string fromAddress, string toAddress, string subject, string message);
+	}
+}
